5a4c3a5c9b55bae75914e3e52c2016133c404e2f
Emir Balic <emirbalic@gmail.com>
2019-06-09 18:23:06 +0200

    skeleton

---
+++ src/Box.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {choice} from './helpers';
+import './Box.css';
+class Box extends Component {
+  static defaultProps = {
+    allColors: [
+      'purple', 'magenta', 'wheat', 'pink', 'violet'
+    ]
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: choice(this.props.allColors) 
+    };
+    this.handleClick = this.handleClick.bind(this);
+  };
+  pickColor() {
+    let nextColor = choice(this.props.allColors);
+    this.setState({color: nextColor})
+  }
+  handleClick() {
+    this.pickColor();
+  }
+  render() {
+    return (
+      <div
+        className="Box"
+        style={{ backgroundColor: this.state.color }}
+        onClick={this.handleClick}
+      />
+    );
+}
+}
+
+export default Box;
